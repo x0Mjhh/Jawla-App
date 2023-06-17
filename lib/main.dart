7cf0d72aff23947980a653dd4e0831ff8387f7ff
Mohammed <dev.mjhh@gmail.com>
2023-06-17 20:35:31 +0300

    details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Screen/detsils_page.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -11,10 +13,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: details_page(),
         ),
-      ),
-    );
+      );
+    
   }
 }
